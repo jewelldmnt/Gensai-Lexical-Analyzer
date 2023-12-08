a782c88c09549a1ad8371b32129352aa80e92acf
Jewell Anne Diamante <jewellanneddiamante@iskolarngbayan.pup.edu.ph>
2023-12-08 17:06:20 +0800

    Added the constant.py

---
+++ constants.py
@@ -0,0 +1,9 @@
+DIGITS = "0123456789"
+KEYWORDS = [
+    "and", "as", "assert", "async", "await", "break", "class", "continue",
+    "def", "del", "elif", "else", "except", "finally", "for", "from", "global",
+    "False", "True", "if", "import", "within", "is", "lambda",
+    "None", "not", "or", "pass", "raise", "repeat", "return", "try", "while",
+    "int", "float", "double", "char", "str", "bool", "out", "in"
+]
+ALPHABETS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
--- lexical_analyzer.py
+++ lexical_analyzer.py
@@ -1,13 +1,4 @@
-DIGITS = "0123456789"
-KEYWORDS = [
-    "and", "as", "assert", "async", "await", "break", "class", "continue",
-    "def", "del", "elif", "else", "except", "finally", "for", "from", "global",
-    "False", "True", "if", "import", "within", "is", "lambda",
-    "None", "not", "or", "pass", "raise", "repeat", "return", "try", "while",
-    "int", "float", "double", "char", "str", "bool", "out", "in"
-]
-ALPHABETS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-
+from constants import *
 
 
 def tokenizer(contents):
