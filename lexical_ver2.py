48971bb32acb26580730e68a25b8067456b41dce
mkas020103 <mdfl020103@gmail.com>
2023-12-25 19:36:04 +0800

    updating version 2

--- lexical_ver2.py
+++ lexical_ver2.py
@@ -11,9 +11,9 @@ def tab_counter(string):
         -tab_tokens (list): A list of tuples, containing the indentation and if its valid or not.
         -emptry list
     '''
+    tab_tokens = []
     # if the string starts with a tab (4 spaces) and has non whitespaces
     if string.startswith('    ') and not string.isspace():
-        tab_tokens = []
         temp_tab = ''
         # iterate over the string per 4 places
         for char in range(0, len(string), 4):
@@ -34,129 +34,51 @@ def tab_counter(string):
             else:
                 break
         return tab_tokens
+    elif string.startswith(' '):
+        whitespace = ''
+        for x in string:
+            if x.isspace():
+                whitespace+=x
+            else:
+                break
+        tab_tokens.append(('invalid_indent', whitespace))
+        return tab_tokens
     else:
         return []
 
-def compound_statement(lexeme):
-    '''
-    compound_statement determine whether a specific lexeme is a compound lexeme or just invalid.
+def categorizer(string, character, inside_quotes):
+    # Its a comment
+    is_partof_comment = not inside_quotes and character == '#'
     
-    Parameters:
-        -lexeme (str): a string that may or may not be a compound statements.
+    # Its a string literal
+    is_start_string = character in ('\'','"')
 
-    Returns:
-        -tab_tokens (list): A list of tuples, containing the indentation and if its valid or not.
-        -emptry list
-    '''
-    tokens = []
-    is_comp = False
-    before_char = ''
-    stringlen = len(lexeme)
-    # iterate over each character
-    for index, char in enumerate(lexeme):
-        if char in SPECIAL_CHAR or (char == '.' and not before_char):
-            tokens.append((classify_lexeme(before_char),before_char))
-            before_char = char
-            continue
-        if char.isalpha() or char == '_':
-            # check if string is still a valid string
-            if before_char.isalnum() and before_char[0].isalpha():
-                before_char += char
-                if stringlen > index + 1:
-                    continue
-                else:
-                    tokens.append((classify_lexeme(before_char),before_char))
-                    continue
-            # else if there is no character
-            elif not before_char:
-                before_char = char
-                if stringlen > index + 1:
-                    continue
-                else:
-                    tokens.append((classify_lexeme(before_char),before_char))
-                    continue
-            else:
-                tokens.append((classify_lexeme(before_char),before_char))
-                before_char = char
-                continue
-        if char.isnumeric():
-            try:
-                if before_char.isnumeric() or not before_char or before_char == '-' or before_char == '.':
-                    before_char += char
-                    if stringlen > index + 1:
-                        continue
-                    else:
-                        tokens.append((classify_lexeme(before_char),before_char))
-                        continue
-                float_value = float(before_char)
-                before_char += char
-                if stringlen > index + 1:
-                    continue
-                else:
-                    tokens.append((classify_lexeme(before_char),before_char))
-                    continue
-            except ValueError:
-                if before_char.isalnum() and before_char[0].isalpha():
-                    before_char += char
-                    if stringlen > index + 1:
-                        continue
-                    else:
-                        tokens.append((classify_lexeme(before_char),before_char))
-                        continue
-                else:
-                    tokens.append((classify_lexeme(before_char),before_char))
-                    before_char = char
-                    continue
-        if char in '*/-+%><^!=':
-            if not before_char:
-                if stringlen >= index + 1:
-                    tokens.append((classify_lexeme(char),char))
-                    continue
-                else:
-                    before_char = char
-                    continue
-            if before_char in '*/-+%><^!=' and char == '=':
-                tokens.append((classify_lexeme(before_char),before_char))
-                before_char = char
-                continue
-            if before_char.isalnum():
-                tokens.append((classify_lexeme(before_char),before_char))
-                before_char = char
-                continue
-            else:
-                # if string length is still greater than current index, append the character before it
-                if stringlen > index + 1:
-                    tokens.append((classify_lexeme(before_char),before_char))
-                    continue
-                else:
-                    # else if lastt state, append both the current and before character/s
-                    tokens.append((classify_lexeme(before_char),before_char))
-                    tokens.append((classify_lexeme(char),char))
-                    continue
-
-        if char == '.':
-            try:
-                int_value = int(before_char)
-                before_char += char
-                if stringlen > index + 1:
-                    continue
-                else:
-                    tokens.append((classify_lexeme(before_char),before_char))
-                    continue
-            except ValueError:
-                tokens.append((classify_lexeme(before_char),before_char))
-                before_char = char
-                if stringlen > index + 1:
-                    continue
-                else:
-                    tokens.append((classify_lexeme(before_char),before_char))
-                    continue
-
-        tokens.append((classify_lexeme(before_char), before_char))
-        before_char = char
-            
-    return is_comp, tokens
-
+    # If not an empty string
+    if string:
+        belongsto_operator = string[0] in '*/-+%><^!=&' and not inside_quotes
+        
+        # It belongs to an identifier
+        belongsto_identifier = (string[0].isalpha() or string[0] == '_') and not inside_quotes
+        
+        # It belongs to a number
+        belongsto_number = (string[0].isnumeric() or string[0] == '.') and not inside_quotes
+    else:
+        belongsto_operator, belongsto_identifier, belongsto_number = False, False, False
+    
+    # Its a logical operator
+    is_operator = character in '*/-+%><^!=&'
+    is_partof_operator = is_operator and belongsto_operator
+    
+    # Its a valid character in an identifier
+    is_identifer = character.isalnum() or character == '_'
+    is_partof_identifier = is_identifer and belongsto_identifier
+    
+    # Its a number or dot
+    is_number = character.isnumeric() or character == '.'
+    is_partof_number = is_number and belongsto_number
+    
+    return is_partof_comment, is_start_string, is_operator, is_partof_operator, is_identifer, is_partof_identifier, is_number, is_partof_number
+        
 def tokenizer(contents):
     """
     Tokenizes the input content into lexemes and classifies them into different token types.
@@ -179,58 +101,77 @@ def tokenizer(contents):
         
         inside_quotes = False  # Flag to track whether the current character is inside quotes or not
         stringlen = len(line)  # Flag to track the length of the current line
-        operator_index = 1     # Flag regarding the number of place the operator character is
-        inside_comment = False # Flag to track if its in a comment
         
         for index, char in enumerate(line):
             # If its part of the lexeme and not the end of a line
-            if not inside_quotes and not char.isspace() and not index == stringlen - 1:
-                # Its a comment
-                is_partof_comment = not inside_quotes and char == '#'
-                # Its a string literal
-                is_partof_string = char in ('\'','"') or inside_quotes
-                # Its an alphabet and the previous string consists of alphabet and numbers and the first character is alphabet (variable or data type)
-                is_partof_identifier = not inside_quotes and not previous_str or ((char.isalnum() or char == '_') and (char[0].isalpha() or char[0] == '_'))
-                # Its a number and the previous string is still a number or part of a number(float or int)
-                is_partof_number = not inside_quotes and not previous_str or ((char.isnumeric() or char == '.') and (char[0].isnumeric() or char[0] == '.'))
-                # Its a logical operator
-                is_partof_operator = not inside_quotes and not previous_str or char in '*/-+%><^!=&'
+            if not char.isspace() and index < stringlen:
+                # Categorize where the character should go
+                (is_partof_comment, is_start_or_end_string, is_operator, is_partof_operator, is_identifer, is_partof_identifier, 
+                is_number, is_partof_number) = categorizer(previous_str, char, inside_quotes)
                 
-                if is_partof_string:
-                    if char in ('\'','"') and inside_quotes:
+                if is_start_or_end_string:
+                    inside_quotes = not inside_quotes
+                    previous_str += char
+                    continue
+                elif inside_quotes:
+                    previous_str += char
+                    continue
+                if is_partof_comment:
+                    tokens.append((classify_lexeme(line[index:]), line[index:]))
+                    previous_str = ""
+                    inside_quotes = False
+                    break
+                if is_identifer:
+                    if is_partof_identifier:
                         previous_str += char
-                        inside_quotes = not inside_quotes
+                        continue
+                    elif previous_str:
                         tokens.append((classify_lexeme(previous_str), previous_str))
-                        break
+                        previous_str = ""
+                        inside_quotes = False
                     else:
                         previous_str += char
                         continue
-                if is_partof_comment:
-                    tokens.append((classify_lexeme(line[index:]), line[index:]))
-                    break
-                if is_partof_identifier:
-                    previous_str += char
-                if is_partof_number:
-                    previous_str += char
-                if is_partof_operator:
-                    if not previous_str:
-                        previous_str = char
-                    elif previous_str in '*/-+%><^!=' and char == '=':
+                if is_operator:
+                    if is_partof_operator:
                         previous_str += char
+                        operator_type = classify_lexeme(previous_str)
+                        if operator_type == 'invalid':
+                            tokens.append((classify_lexeme(previous_str[0]), previous_str[0]))
+                            tokens.append((classify_lexeme(char), char))
+                        else:
+                            tokens.append((operator_type, previous_str))
+                        previous_str = ""
+                        inside_quotes = False
+                        continue
+                    elif (is_identifer or is_partof_comment or inside_quotes or is_number) and previous_str:
                         tokens.append((classify_lexeme(previous_str), previous_str))
                         previous_str = ""
+                        inside_quotes = False
                         continue
+                    else:
+                        if not index == stringlen - 1:
+                            previous_str += char
+                            continue
+                        else:
+                            tokens.append((classify_lexeme(char), char))
+                            previous_str = ""
+                            inside_quotes = False
+                            continue
+                if is_number:
+                    if is_partof_number:
+                        previous_str += char
+                        continue
+                    else:
+                        tokens.append((classify_lexeme(previous_str), previous_str))
+                        previous_str = ""
+                        inside_quotes = False
+
             else:
+                if not previous_str or previous_str.isspace():
+                    continue
                 # classify lexeme
-                lexeme_type = classify_lexeme(line)
-                # if invalid check if its compound
-                if lexeme_type == 'invalid':
-                    # if compound combine the tokens
-                    compound_tokens = compound_statement(line)
-                    tokens = tokens + compound_tokens
-                else:
-                    tokens.append((lexeme_type, line))
-                    
+                tokens.append((classify_lexeme(previous_str), previous_str))
 
         all_tokens.append(tokens)
     return all_tokens
