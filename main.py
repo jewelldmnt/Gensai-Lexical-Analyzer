76f24d7db486d52bdc815f4be22e466b2a4d54ca
Jewell Anne Diamante <jewellanneddiamante@iskolarngbayan.pup.edu.ph>
2023-12-26 15:50:06 +0800

    Modified the main.py to check file extension

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ def main():
     tokens = parse(file_path)
     data = []
 
+    if not file_path.endswith(".gs"):
+        print("Error: Invalid file extension. Only '.gs' files are allowed.")
+        return
+
+
     for line_num, line_code in enumerate(tokens, start=1):
         for token, lexeme in line_code:
             data.append([line_num, token, lexeme])
