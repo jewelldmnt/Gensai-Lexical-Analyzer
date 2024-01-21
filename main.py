f8fd7853df2a89ba25edc2a74048c35915418e5c
Jewell Anne Diamante <jewellanneddiamante@iskolarngbayan.pup.edu.ph>
2024-01-21 22:29:46 +0800

    Updated the main.py

--- main.py
+++ main.py
@@ -14,31 +14,28 @@ def main():
     Returns:
         pdf file or csv file: data containing the tokens, type of token, and what line it is found on.
     '''
-    file_name = input("Enter the file name (without the path, e.g., 'example.gsai'): ")
-    file_path = f'tester/{file_name}'  # Assuming the files are always inside the 'tester' folder
-
     try:
-        tokens = parse(file_path)
+        file_name = input("Enter the file name (without the path, e.g., 'example.gsai'): ")
+        file_path = f'tester/{file_name}'  # Assuming the files are always inside the 'tester' folder
+        number_dot = 0
+        for char in file_name:
+            if (char in SPECIAL_CHAR or char in OPERATORS) and char !='.':
+                print("Error: Invalid file extension. Don't include special character.")
+                return
+            if char == '.':
+                number_dot += 1
+            if number_dot >= 2:
+                print("Error: Invalid file extension. You must have only one file extension.")
+                return
+
+        if not file_path.endswith(".gsai"):
+            print("Error: Invalid file extension. Only '.gsai' files are allowed.")
+            return
     except:
         print(f"Error: File '{file_path}' not found.")
         return
-    
-    data = []
-    
-    number_dot = 0
-    for char in file_name:
-        if (char in SPECIAL_CHAR or char in OPERATORS) and char !='.':
-            print("Error: Invalid file extension. Don't include special character.")
-            return
-        if char == '.':
-            number_dot += 1
-        if number_dot >= 2:
-            print("Error: Invalid file extension. You must have only one file extension.")
-            return
 
-    if not file_path.endswith(".gsai"):
-        print("Error: Invalid file extension. Only '.gsai' files are allowed.")
-        return
+    tokens = parse(file_path)
 
     table = prettytable.PrettyTable(["Line Number", "Lexeme", "Token"])
     table.align["Lexeme"] = "l"
